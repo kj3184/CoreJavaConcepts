30b03ee61098816ef2e5ddfb1e8d2859fb67ca97
kj3184 <kunal.jadhav84@gmail.com>
2025-01-02 22:49:17 +0800

    Added dev package

---
+++ src/dev/lpa/Main.java
@@ -0,0 +1,18 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dev.lpa.domain.Employee;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		List<Employee> employees=new ArrayList<>(List.of(
+				new Employee(10001, "Ralph", 2015),
+				null, null, null, null, null, null, null, null, null))
+
+	}
+
+}
---
+++ src/dev/lpa/domain/EmployeeComparator.java
@@ -0,0 +1,8 @@
+package dev.lpa.domain;
+
+import java.util.Comparator;
+import java.util.concurrent.ConcurrentMap;
+
+public class EmployeeComparator <T extends Employee> implements Comparator<Employee>{
+
+}
